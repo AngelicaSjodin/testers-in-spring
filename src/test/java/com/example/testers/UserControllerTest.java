94763166392cc3bfc31e4f5a731f34e9ce53f3f3
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-04-26 20:45:34 +0200

    controller test, before each method

--- src/test/java/com/example/testers/UserControllerTest.java
+++ src/test/java/com/example/testers/UserControllerTest.java
@@ -1,7 +1,21 @@
 package com.example.testers;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import org.junit.jupiter.api.BeforeEach;
 
-class UserControllerTest {
+
+    class UserControllerTest {
+
+    private UserRepository mockedRepo;
+    private UserService userService;
+    private UserController userController;
+
+    //runs this code once first
+    @BeforeEach
+    public void setUp(){
+        mockedRepo = mock(UserRepository.class);
+        userService = new UserService(mockedRepo);
+        userController = new UserController(userService);
+    }
 
 }
